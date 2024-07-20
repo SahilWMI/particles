328cf08da2b67cf09be211aff9c8a6655ca12be4
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-20 23:47:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+console.log(ctx);
+
+class Particle {
+
+}
+
+class Effect {
+
+}
+
+function animate(){
+
+}
