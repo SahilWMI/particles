6b8a757f2c96e2d2cebe9710c2e8d0268ae7b81e
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-20 23:54:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ class Particle {
         this.y = Math.random() *this.effect.height;
         this.radius = 15;
     }
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI *2);
+        context.fill;
+    }
 }
 
 class Effect {
-      constructor(effect){
+    constructor(effect){
         this.canvas =canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
